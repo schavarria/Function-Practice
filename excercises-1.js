a3fc6b905beed69a4c5f019e76b80f00980002fc
Sucely Chavarria <schavarria7@yahoo.com>
2018-12-05 08:45:07 -0800

    hw

--- excercises-1.js
+++ excercises-1.js
@@ -32,58 +32,86 @@
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
 
+function sum(a,b){
+	return a+b
+}
+
 console.assert(sum(2,4) === 6)
 console.assert(sum(45, 23) === 68)
 console.assert(sum(-2, -57) === -59)
+ 
 
 // Q2
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
+function avg(a,b,c) {
+	return (a+b+c) /3
+}
+
 console.assert(avg(2,3,4) === 3)
 console.assert(avg(234, 63, 234) === 177)
 console.assert(avg(123,234,345) === 234)
 
-// Q3
-// Write a function called `getLength` that takes one
-// parameter (a string) and returns the length
+// // Q3
+// // Write a function called `getLength` that takes one
+// // parameter (a string) and returns the length
+
+function getLength(a){
+	return a.length
+}
 
 console.assert(getLength('Michael') === 7)
 console.assert(getLength('Javascript') === 10)
 console.assert(getLength('supercalifragilisticexpialidocious') === 34)
 
-// Q4
-// Write a function called `greaterThan` that takes
-// two parameters and returns `true` if the first
-// parameter is greater than the second. Otherwise
-// the function should return `false`.
+// // Q4
+// // Write a function called `greaterThan` that takes
+// // two parameters and returns `true` if the first
+// // parameter is greater than the second. Otherwise
+// // the function should return `false`.
+
+function greaterThan(a,b){
+	if (a>b) {
+		return true
+	}else { 
+		return false}
+}
 
 console.assert(greaterThan(2, 1) === true)
 console.assert(greaterThan(23, 23) === false)
 console.assert(greaterThan(-234, 1) === false)
 
-// Q5
-// Write a function called `greet` that takes a
-// single parameter and returns a string that
-// is formated like "Hello, Name!" where *Name*
-// is the parameter that was passed in.
+// // Q5
+// // Write a function called `greet` that takes a
+// // single parameter and returns a string that
+// // is formated like "Hello, Name!" where *Name*
+// // is the parameter that was passed in.
+
+function greet(a){
+	return `Hello, ${a}!`
+}
 
 console.assert(greet("Michael") === "Hello, Michael!")
 console.assert(greet("R2D2") === "Hello, R2D2!")
 console.assert(greet("Iron Man") === "Hello, Iron Man!")
 
-// Q6
-// Write a function called `madlib` that takes
-// 4 or more parameters (words). The function 
-// should insert the words into a pre-defined
-// sentence. Finally the function should return
-// that sentence.
-// Note: When I say words and sentence I mean
-// strings. For example:
-// words: "quick", "fox", "fence"
-// sentence: "quick brown fox jumps over the fence"
+// // Q6
+// // Write a function called `madlib` that takes
+// // 4 or more parameters (words). The function 
+// // should insert the words into a pre-defined
+// // sentence. Finally the function should return
+// // that sentence.
+// // Note: When I say words and sentence I mean
+// // strings. For example:
+// // words: "quick", "fox", "fence"
+// // sentence: "quick brown fox jumps over the fence"
+
+function madlib (a,b,c,d){
+	return `The ${a} brown ${b} ${c} over the ${d}.`
+}
 
 console.assert(madlib("quick","fox","jumps","fence")=== "The quick brown fox jumps over the fence.")
 console.assert(madlib("fat","rabbit","hops","wall")=== "The fat brown rabbit hops over the wall.")
-///////////////////////////////////////////////////////
-///////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////
--- excercises-2.js
+++ excercises-2.js
@@ -1,27 +1,54 @@
-// ---------------------
-// Define a function max() that takes two numbers as 
-// arguments and returns the largest of them. Use the 
-// if-then-else construct available in JavaScript.
-// ---------------------
+
+// // ---------------------
+// // Define a function max() that takes two numbers as 
+// // arguments and returns the largest of them. Use the 
+// // if-then-else construct available in JavaScript.
+// // ---------------------
+
+function max(a,b){
+	if(a>b){
+		return a 
+	}else {return b}
+}
 
 console.assert(max(2,3) === 3)
 console.assert(max(23,24) === 24)
 console.assert(max(-23, 12) === 12)
 
-// ---------------------
-// Define a function maxOfThree() that takes three 
-// numbers as arguments and returns the largest of them.
-// ---------------------
+// // ---------------------
+// // Define a function maxOfThree() that takes three 
+// // numbers as arguments and returns the largest of them.
+// // ---------------------
+
+function maxOfThree(a,b,c){
+	if(a>b && a>c){
+		return a
+	} if (b>a && b>c){
+		return b
+	}else { return c}
+
+	
+}
 
 console.assert(maxOfThree(2,56,3) === 56)
 console.assert(maxOfThree(12,3,4) === 12)
 console.assert(maxOfThree(-12,4,-5) === 4)
 
-// ---------------------
-// Write a function isVowel() that takes a character (i.e. a 
-// string of length 1) and returns true if it is a 
-// vowel, false otherwise.
-// ---------------------
+// // ---------------------
+// // Write a function isVowel() that takes a character (i.e. a 
+// // string of length 1) and returns true if it is a 
+// // vowel, false otherwise.
+// // ---------------------
+
+function isVowel(char){
+var vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U' ]
+if (vowels.indexOf(char) == -1) {
+	return false
+}else {
+	return true
+}
+
+}
 
 console.assert(isVowel(0) === false);
 console.assert(isVowel("B") === false);
@@ -30,13 +57,29 @@ console.assert(isVowel("a") === true);
 console.assert(isVowel("E") === true);
 console.assert(isVowel("2") === false)
 
-// ---------------------
-// Write a function rovarspraket() that will translate 
-// a text into "Rovarspraket". That is, double every 
-// consonant and place an occurrence of "o" in between. 
-// For example, translate("this is fun") should return 
-// the string "tothohisos isos fofunon".
-// ---------------------
+// // ---------------------
+// // Write a function rovarspraket() that will translate 
+// // a text into "Rovarspraket". That is, double every 
+// // consonant and place an occurrence of "o" in between. 
+// // For example, translate("this is fun") should return 
+// // the string "tothohisos isos fofunon".
+// // ---------------------
+
+function rovarspraket(a){
+	var newWord = ""
+	if (typeof a !== "string"){
+		return a + ''
+	}
+for (let index=0; index<a.length; index+=1)	{
+	if(isVowel(a[index])){
+		newWord+=a[index]
+	}else { 
+		newWord+=a[index] + 'o' + a[index]
+	}
+}
+return newWord
+}
+
 
 console.assert(rovarspraket("a") === "a")
 console.assert(rovarspraket("b") === "bob")
@@ -45,12 +88,23 @@ console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
 console.assert(rovarspraket(0) === "0")
 
 
-// ---------------------
-// Define a function reverse() that computes the reversal 
-// of a string. For example, reverse("jag testar") should 
-// return the string "ratset gaj".
-// ---------------------
+// // ---------------------
+// // Define a function reverse() that computes the reversal 
+// // of a string. For example, reverse("jag testar") should 
+// // return the string "ratset gaj".
+// // ---------------------
+
+function reverse(a){
+	return a.split('').reverse().join('')
+}
+
 
 
 console.assert(reverse("books") === "skoob")
 console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
+
+
+
+
+
+
